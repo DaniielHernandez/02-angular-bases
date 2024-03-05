249d0cd04d575643cab4ad0436e83a98011d5a9a
Daniel Hernandez <danielhernandezupiita@gmail.com>
2024-03-05 12:09:30 -0600

    Fin de sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+
+  public title: string = 'Hola Mundo';
+
+
 }
